e6cb9150f2ab095d441b7868001770f3ec455960
mac <martinnjoroge0028@gmail.com>
2024-06-16 16:09:32 +0300

    Modified title

--- task-app/resources/views/tasks.blade.php
+++ task-app/resources/views/tasks.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>Drag and Drop Sortable List</title>
+  <title>Drag and Drop Tasks App</title>
 
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
